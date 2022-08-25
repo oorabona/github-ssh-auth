9120c65e05d85e15670857b1b75c589b11bb14c2
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-08-25 15:05:39 +0200

    refact: setuptools config now in cfg file

--- setup.py
+++ setup.py
@@ -1,85 +1,17 @@
-# -*- coding: utf-8 -*-
+import textwrap
 
-"""Use your GitHub organization user's keys as a SSH authentication method."""
-
-from __future__ import absolute_import, division, print_function
-
-from setuptools import setup, find_packages
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup_requires = [
-    'autosemver>=1.0.0',
-]
-
-install_requires = [
-    'click>=7.0',
-    'PyGithub>=1.2.0',
-    'configparser',
-    'autosemver>=1.0.0',
-]
-
-docs_require = []
-
-tests_require = [
-    'flake8',
-    'pytest',
-    'pytest-cov',
-    'coverage',
-]
-
-extras_require = {
-    'docs': docs_require,
-    'tests': tests_require,
-}
-
-extras_require['all'] = []
-for name, reqs in extras_require.items():
-    if name not in ['all']:
-        extras_require['all'].extend(reqs)
-
-packages = find_packages(exclude=['docs', 'tests'])
-url = "https://github.com/oorabona/github-ssh-auth"
+from setuptools import setup
 
 setup(
-    name="github-ssh-auth",
-    autosemver={
-        'bugtracker_url': url + '/issues',
+    use_scm_version={
+        "write_to": "github_ssh_auth/version.py",
+        "write_to_template": textwrap.dedent(
+            """
+            # coding: utf-8
+            from __future__ import unicode_literals
+            __version__ = {version!r}
+            """,
+        ).lstrip(),
     },
-    author="Olivier Orabona",
-    author_email="olivier.orabona@gmail.com",
-    description="Authenticate SSH users using their GitHub keys",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url=url,
-    packages=packages,
-    include_package_data=True,
-    setup_requires=setup_requires,
-    install_requires=install_requires,
-    tests_require=tests_require,
-    extras_require=extras_require,
-    entry_points={
-        'console_scripts': [
-            'github-ssh = github_ssh_auth.cli:cli',
-            'github-ssh-update = github_ssh_auth.cli:update',
-            'github-ssh-auth = github_ssh_auth.cli:auth',
-            # 'github-ssh-init = github_ssh_auth.cli:init',
-        ],
-    },
-    classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-        "Topic :: System :: Systems Administration",
-        "Topic :: System :: Systems Administration :: Authentication/Directory",
-        "Topic :: System :: Shells",
-        "Topic :: Security"
-    ],
+    package_dir={"": "github_ssh_auth"},
 )
