6945b9187eef782a1d4cd262127d55e4a8077d44
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-08-26 01:56:27 +0200

    feat: implement a release version based on github refname

---
+++ release.py
@@ -0,0 +1,17 @@
+import textwrap
+import codecs
+import os
+from setuptools import setup
+
+gh_ref_name = os.getenv("GITHUB_REF_NAME")
+version_py = textwrap.dedent(
+    f"""
+    # coding: utf-8
+    from __future__ import unicode_literals
+    __version__ = {gh_ref_name}
+    """
+)
+
+print("Setting version file to '{}'".format(gh_ref_name))
+with codecs.open("github_ssh_auth/version.py", "a", "utf-8") as file_handler:
+    file_handler.write(version_py)
