7f485ec7caf2bea252762b0d33ea14d2751b7dd9
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-08-27 18:19:56 +0200

    wip on auto semantic versionning

--- src/github_ssh_auth/_static_version.py
+++ src/github_ssh_auth/_static_version.py
@@ -5,23 +5,38 @@
 # distribution is made.  The magic value "__use_git__" is interpreted by
 # _version.py.
 
-import os
-from collections import namedtuple
-
-Version = namedtuple("Version", ("release", "dev", "labels"))
-
-is_ci = os.getenv("CI")
-
 version = "__use_git__"
 # default_version = "0.0.0"
 
 
 def default_version():
-    global is_ci
-    global Version
+    import os
+    import re
+    from collections import namedtuple
+
+    import requests
+    from packaging import version
+
+    r = requests.get("https://test.pypi.org/pypi/github-ssh-auth/json")
+    r.raise_for_status()
+
+    latest_version = r.json()["info"]["version"]
+
+    Version = namedtuple("Version", ("release", "dev", "labels"))
+
+    is_ci = os.getenv("CI")
+
     # Set local version when we have a branch (ref_name is not a tag)
     if is_ci:
-        return Version("0.0.0.dev", 0, "")
+        # Get N in devN version string
+        result = re.search(version.VERSION_PATTERN, latest_version)
+        dev = result.group("dev") if result else None
+        # Increment dev version
+        if dev:
+            dev = int(dev) + 1
+        else:
+            dev = 0
+        return Version("0.0.0.dev", dev, "")
     return Version("0.0.0.dev", 0, None)
 
 
