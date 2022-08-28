08f990aac38d543b3d6402d24184f36b2d2d127a
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-08-28 02:00:54 +0200

    fix retrieving correct version from PyPI repository

--- src/github_ssh_auth/_static_version.py
+++ src/github_ssh_auth/_static_version.py
@@ -21,7 +21,12 @@ def default_version():
     r = requests.get("https://test.pypi.org/pypi/github-ssh-auth/json")
     r.raise_for_status()
 
-    latest_version_str = r.json()["info"]["version"]
+    # Actually retrieving the latest version from PyPI is a bit of a pain.
+    # We cannot fully rely on info.version for the latest version, because
+    # it seems to be only the "base" version, not the "full" (i.e. public) version.
+    # So we have to do some extra work to get the latest version from the 'releases' instead.
+    # latest_version_str = r.json()["info"]["version"]
+    latest_version_str = [k for k in r.json()["releases"].keys()][-1]
 
     is_ci = os.getenv("CI_RUN")
 
@@ -31,11 +36,11 @@ def default_version():
         latest_version = parse(latest_version_str)
         dev = latest_version.dev
         # Increment dev version
-        if dev:
+        if dev is not None:
             dev = int(dev) + 1
         else:
             dev = 0
-        return Version(f"{latest_version.public}.dev", dev, "")
+        return Version(f"{latest_version.base_version}.dev", dev, "")
     return Version("0.0.0.dev", 0, None)
 
 
