0af9219a52289e08a71114870a3c09e555f062b8
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-09-07 16:29:38 +0200

    remove unneeded import

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup
 
 # Loads _version.py module without importing the whole package.
 def get_version_and_cmdclass(pkg_path):
-    import os
     from importlib.util import module_from_spec, spec_from_file_location
 
     spec = spec_from_file_location(
